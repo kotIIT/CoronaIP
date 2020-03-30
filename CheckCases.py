2413c1cabd014ced14634a2ea143bf8f17ca968e
kotIIT <mkotlyar@hawk.iit.edu>
2020-03-30 17:34:22 -0500

    pprint

--- CheckCases.py
+++ CheckCases.py
@@ -1,13 +1,28 @@
-import json, urllib.request, pprint
+import json
+import requests
+import zipcodes
+from pprint import pprint
+import ipinfo
 
+with requests.get("https://dph.illinois.gov/sites/default/files/COVID19/COVID19CountyResults20200330_2.json") as response:
 
-with urllib.request.urlopen("https://dph.illinois.gov/sites/default/files/COVID19/COVID19CountyResults20200330_2.json") as url:
-    data = json.loads(url.read())
-    for c in data["characteristics_by_county"]:
-        print(f'County  {c["values"]["County"]}')
+    # for c in data["characteristics_by_county"]:
+    print(response.json())
+    data = response.json()['characteristics_by_county']['values']
+    for item in data:
+        # pprint(item)
+        if item['County'] == 'Illinois':
+            print("BUBBUBUBUBUBUBBUBUBUB")
+            pprint(item)
 
-        # print(f 'County:{c["values"[]]County})
-        # print('Cases: ' + c['confirmed_cases'])
-        # print('Deaths: ' + c['deaths'])
-        # print('')
+            print(item['confirmed_cases'])
 
+    # pprint(data)
+
+# PublicIP = requests.get("https://ipinfo.io")
+#
+# print(PublicIP)
+# print(PublicIP.json())
+
+#zipcodes.matching  ()
+#from geopy.geocoders import Nominatim
