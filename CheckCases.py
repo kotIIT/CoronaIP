0392f6132e17d7cd58892330bf1ecb9a3edeaf82
kotIIT <mkotlyar@hawk.iit.edu>
2020-03-30 17:53:08 -0500

    pprint virus location

--- CheckCases.py
+++ CheckCases.py
@@ -4,25 +4,48 @@ import zipcodes
 from pprint import pprint
 import ipinfo
 
-with requests.get("https://dph.illinois.gov/sites/default/files/COVID19/COVID19CountyResults20200330_2.json") as response:
 
-    # for c in data["characteristics_by_county"]:
-    print(response.json())
-    data = response.json()['characteristics_by_county']['values']
-    for item in data:
-        # pprint(item)
-        if item['County'] == 'Illinois':
-            print("BUBBUBUBUBUBUBBUBUBUB")
-            pprint(item)
+def get_covid_info_for_county_ILLINOIS(county: str) -> dict:
+    with requests.get(
+            "https://dph.illinois.gov/sites/default/files/COVID19/COVID19CountyResults20200330_2.json") as response:
 
-            print(item['confirmed_cases'])
+        # for c in data["characteristics_by_county"]:
+        # pprint(response.json())
+        data = response.json()['characteristics_by_county']['values']
+        for item in data:
+            # pprint(item)
+            if item['County'].upper() == county.upper():
+                # print("BUBBUBUBUBUBUBBUBUBUB")
+                return (item)
 
-    # pprint(data)
+                # print(f"poop fuck: {item['confirmed_cases']}")
 
-# PublicIP = requests.get("https://ipinfo.io")
-#
-# print(PublicIP)
-# print(PublicIP.json())
+    return None
 
-#zipcodes.matching  ()
-#from geopy.geocoders import Nominatim
+
+def get_county_from_zip(zip: str) -> dict:
+    return zipcodes.matching(zip)
+
+
+def get_county_from_current_ip() -> str:
+    PublicIP = requests.get("https://ipinfo.io")
+
+    # print(PublicIP)
+    # pprint(PublicIP.json())
+
+    zipcode_data = (zipcodes.matching(PublicIP.json()['postal']))
+
+    # pprint(zipcode_data)
+
+    county = zipcode_data[0]['county'].upper().replace("COUNTY", '').strip()
+
+    return county
+
+
+if __name__ == '__main__':
+    # pprint(get_covid_info_for_county_ILLINOIS('lake'))
+
+    print(get_county_from_current_ip())
+
+    # zipcodes.matching  ()
+    # from geopy.geocoders import Nominatim
