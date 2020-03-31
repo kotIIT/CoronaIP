2bf04ca128d8ba570421b0c406290cb22e6925bf
Henry Post <HenryFBP@gmail.com>
2020-03-31 18:53:57 -0500

    query api correctly

--- webApiAndClient/app.py
+++ webApiAndClient/app.py
@@ -2,14 +2,18 @@ from flask import Flask, jsonify
 import os
 import sys
 
-sys.path.append('../..')  # ugly hack TODO lol dont fix it :)
+sys.path.append('..')  # ugly hack TODO lol dont fix it :)
 import CheckCases
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static/')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route('/api/health/')
-@app.route('/')
 def health():
     return jsonify({'message': 'Hello, World! I am a web API.',
                     'health': 'OK'})
