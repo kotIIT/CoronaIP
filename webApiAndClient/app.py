73e1ba22b7ac50ccac50f977fb1b5637e45af5f3
Henry Post <HenryFBP@gmail.com>
2020-03-31 19:15:19 -0500

    add get covid info by county

--- webApiAndClient/app.py
+++ webApiAndClient/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 import sys
 
@@ -6,6 +6,29 @@ sys.path.append('..')  # ugly hack TODO lol dont fix it :)
 import CheckCases
 
 app = Flask(__name__, static_url_path='/static/')
+
+
+class InvalidUsage(Exception):  # from https://flask.palletsprojects.com/en/1.1.x/patterns/apierrors/
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 @app.route('/')
@@ -19,9 +42,17 @@ def health():
                     'health': 'OK'})
 
 
-@app.route('/api/getCountyByZIP')
-def getCountyByMyIP():
-    return jsonify({'to': 'do'})
+@app.route('/api/getCovidJSONByIllinoisCounty')
+def getCovidJSONByIllinoisCounty():
+    if 'county' not in request.args:
+        raise InvalidUsage('You must specify the "county" parameter', status_code=422)
+
+    try:
+        result: dict = (CheckCases.get_covid_info_for_county_ILLINOIS(request.args.get('county')))
+    except CheckCases.CountyNotFoundException as e:
+        raise InvalidUsage('County "' + request.args.get('county') + '" does not exist!')
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
